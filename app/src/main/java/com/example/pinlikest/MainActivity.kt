4d5417f74e657f620c4d4d5186d6af575d32774f
PPelegrii <pedropelegri2005@gmail.com>
2025-08-25 11:29:16 -0300

    adicionado homescreen

--- app/src/main/java/com/example/pinlikest/MainActivity.kt
+++ app/src/main/java/com/example/pinlikest/MainActivity.kt
@@ -1,27 +1,53 @@
 package com.example.pinlikest
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,7 +60,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PinlikestTheme {
-                    TelaLogo()
+                HomeScreen()
                 }
             }
         }
@@ -45,7 +71,8 @@ class MainActivity : ComponentActivity() {
 fun TelaLogo() {
     Column(
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
             .background(color = Color.White)
     ) {
         Row(
@@ -86,4 +113,150 @@ fun TelaLogo() {
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        //containerColor = Color.Black,
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = { Text("Para Você") }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+
+                ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                ) {
+                    IconButton(onClick = {
+                        Log.d("botaoHome", "usuario-clicouHome_route")
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Home,
+                            contentDescription = "",
+                            modifier = Modifier.size(40.dp)
+                        )
+                    }
+                    IconButton(onClick = {
+                        Log.d("botaoSearch", "usuario-clicouSearch_route")
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "",
+                            modifier = Modifier.size(40.dp)
+                        )
+                    }
+                    IconButton(onClick = {
+                        Log.d("botaoCreate/Upload", "usuario-clicouCreate/Upload_route")
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = "",
+                            modifier = Modifier.size(40.dp)
+                        )
+                    }
+                    IconButton(onClick = {
+                        Log.d("botaoMessages", "usuario-clicouMessages_route")
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.MailOutline,
+                            contentDescription = "",
+                            modifier = Modifier.size(40.dp)
+                        )
+                    }
+                    IconButton(onClick = {
+                        Log.d("botaoUserProfile", "usuario-clicouUserProfile_route")
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.AccountCircle,
+                            contentDescription = "",
+                            modifier = Modifier.size(40.dp)
+                        )
+                    }
+                }
+            }
+        },
+        content = { paddingValues ->
+            Row(
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.Top,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(paddingValues)
+            ) {
+                Column(
+                    Modifier
+                        .padding(8.dp)
+                        .background(color = Color.White)
+                        .weight(1f)
+                ) {
+                    PinHome(image = R.drawable.temp1, descricao  = "eu")
+                    PinHome(image = R.drawable.temp4, descricao  = "não")
+                    PinHome(image = R.drawable.temp3, descricao  = "acredito")
+                    PinHome(image = R.drawable.temp4, descricao  = "nisso")
+                }
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.padding(8.dp)
+                        .background(color = Color.White)
+                        .weight(1f)
+                ) {
+                    PinHome(image = R.drawable.temp5, descricao  = "blah")
+                    PinHome(image = R.drawable.temp2, descricao  = "blah")
+                    PinHome(image = R.drawable.temp1, descricao  = "blah")
+                    PinHome(image = R.drawable.temp4, descricao  = "blah")
+                }
+            }
+        }
+    )
+}
+@Composable
+fun PinHome(image: Int, descricao: String) {
+    Image(
+        painter = painterResource(image),
+        contentDescription = "ImagePin",
+        modifier = Modifier
+            .fillMaxWidth()
+    )
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(25.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .padding(2.dp)
+                .fillMaxWidth(),
+        ) {
+            Text(descricao)
+            Icon(imageVector = Icons.Default.MoreVert, contentDescription = "")
+        }
+    }
+    Spacer(modifier = Modifier.height(8.dp))
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PinDetails() {
+
 }
